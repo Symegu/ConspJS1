66f0ea851caa1030488d08777c2c36cad072e81e
Symegu <89914120+Symegu@users.noreply.github.com>
2022-01-22 14:59:28 +0300

    подготовка

---
+++ script2.js
@@ -0,0 +1,6 @@
+2.1 - https://youtu.be/7oFxqoDQ-Tw
+2.2 - https://youtu.be/KgffVAdLSoo
+2.3 - https://youtu.be/JRF2zbyDotE
+2.4 - https://youtu.be/IwWdRhh93CA
+2.5 - https://youtu.be/cRTBa4G58Mc
+2.6 - https://youtu.be/Hj5eBubSank
